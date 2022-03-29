fdcabc66bd6b9b103df18456202b18060bb44247
PeoplesChamp46 <sasukemestry@gmail.com>
2022-03-29 18:55:04 +0530

    heroku update 3

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/blackCurrent'));
+app.use(express.static(__dirname + '/dist/black-current'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/blackCurrent/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/black-current/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
